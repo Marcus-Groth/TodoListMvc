740a59ad59c97302cd0fb64c9cdd4a1478b43182
rodersnow <rodersnow@gmail.com>
2024-09-10 15:04:52 +0200

    feat(TaskItem): enforce unique constraint on TaskItem Title property

--- Contexts/AppDbContext.cs
+++ Contexts/AppDbContext.cs
@@ -9,4 +9,13 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<TaskItem> TaskItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<TaskItem>()
+                .HasIndex(item => item.Title)
+                .IsUnique();
+    }
 }
---
+++ Migrations/20240910125715_TaskItemsPropertyTitleIsUnique.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoListMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskItemsPropertyTitleIsUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskItems_Title",
+                table: "TaskItems",
+                column: "Title",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TaskItems_Title",
+                table: "TaskItems");
+        }
+    }
+}
